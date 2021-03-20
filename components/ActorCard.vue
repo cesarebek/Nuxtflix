650a48d673edb8d1a1911a0fc6d5f1d5bc124869
Cesare Bek <cesarebek1@gmail.com>
2021-03-20 21:20:05 +0100

    fixed actor image for safari

--- components/ActorCard.vue
+++ components/ActorCard.vue
@@ -3,7 +3,9 @@
     @click="showDetails"
     class="flex justify-center w-1/2 sm:w-1/3 shadow-md md:w-1/4 lg:w-1/6 relative flex-shrink-0 overflow-hidden rounded-md my-3 cursor-pointer"
   >
-    <img class="h-auto" :src="coverPath" alt="cover" />
+    <div>
+      <img :src="coverPath" alt="cover" />
+    </div>
     <div class="absolute bottom-0">
       <p class="text-shadow-xl uppercase text-center">{{ name }}</p>
     </div>
